3d7c97bfcc406108ba3b0249cbd79d0231617a74
Matt0105 <matteo0105@gmail.com>
2022-01-17 16:04:06 +0100

    scaffholding

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/scss/style.scss";
+
+#app {
+ 
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+      <nav>
+          <img src="../assets/img/17-176153_download-logo-spotify-png.png" alt="">
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/partials/_variables.scss";
+
+    nav {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        height: 60px;
+        background-color: $light-blue;
+
+        img {
+            height: 85%;
+            margin-left: 5px;
+        }
+    }
+
+    
+</style>
