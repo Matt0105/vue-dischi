f82785c70274bad81a14e0b01325b0374a7351e3
Matt0105 <matteo0105@gmail.com>
2022-01-17 16:27:28 +0100

    first static card

--- src/components/CardElement.vue
+++ src/components/CardElement.vue
@@ -1,5 +1,14 @@
 <template>
-  
+  <div class="card">
+      <div class="thumb">
+          <img src="https://picsum.photos/200" alt="">
+      </div>
+      <div class="card-info">
+          <h2>Title</h2>
+          <h3>Artist</h3>
+          <h4>Year</h4>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +17,52 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import "../assets/scss/partials/_variables.scss";
+
+    .card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: calc(100% / 5);
+        height: calc(100% / 2);
+        padding-top: 2rem;
+        background-color: $light-blue;
+
+        .thumb {
+            width: 100%;
+            text-align: center;
+
+            img {
+                width: 70%;
+            }
+        }
+
+        .card-info {
+            color: white;
+            margin-top: 1rem;
+            text-align: center;
+
+            h2 {
+                text-transform: uppercase;
+                font-size: 2rem;
+                margin-bottom: 1.5rem;
+            }
+
+            h3, h4 {
+                color: gray;
+                font-weight: 500;
+            }
+
+            h3 {
+                font-size: 1.5rem;
+            }
+
+            h4 {
+                font-size: 1.2rem;
+            }
+        }
+    }
+
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,33 @@
 <template>
   <main>
-      
+    <div class="container">
+      <CardElement />
+    </div>
   </main>
 </template>
 
 <script>
+import CardElement from './CardElement.vue'
 export default {
+  components: { CardElement },
     name: "Main",
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/scss/partials/_variables.scss";
+
+  main {
+    width: 100%;
+    height: calc(100vh - 60px);
+    background-color: $dark-blue;
+  }
+
+  .container {
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+    padding-top: 3rem;
+  }
 
 </style>
